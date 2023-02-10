737c198ff77152b482922fda5b7a4922f5a17bf3
IschukM <dzubak@ukr.net>
2023-02-10 23:19:14 +0200

    Profile

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,32 @@
+const Profile = ({ username, tag, location }) => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          alt="User avatar"
+          className="avatar"
+        />
+        <p className="name">{username}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      {/* <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">stats.likes</span>
+        </li>
+      </ul> */}
+    </div>
+  );
+};
+export default Profile;
