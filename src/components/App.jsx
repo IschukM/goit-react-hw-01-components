adaf32c1546147a9093de4132edf570c4d6714df
IschukM <dzubak@ukr.net>
2023-02-12 21:53:03 +0200

    stats

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Profile } from './Profile/Profile';
 import user from 'user.json';
+import { Statistics } from './Statistics/Statistics';
+import data from 'data.json';
 export const App = () => {
   return (
     <div
@@ -21,6 +23,7 @@ export const App = () => {
         views={user.stats.views}
         likes={user.stats.likes}
       />
+      <Statistics title={data.title} stats={data.stats} />
     </div>
   );
 };
