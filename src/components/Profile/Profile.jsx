d036fcf0fd0e74af1535951cf92623aa10b39084
IschukM <dzubak@ukr.net>
2023-02-12 21:20:40 +0200

    css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,6 @@
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
+
 export const Profile = ({
   avatar,
   username,
@@ -8,28 +11,38 @@ export const Profile = ({
   likes,
 }) => {
   return (
-    <div className="profile">
-      <div className="description">
-        <img src={avatar} alt="User avatar" className="avatar" />
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
         <p className="name">{username}</p>
         <p className="tag">{tag}</p>
         <p className="location">{location}</p>
       </div>
 
-      <ul className="stats">
-        <li>
+      <ul className={css.stats}>
+        <li className={css.stats__item}>
           <span className="label">Followers</span>
           <span className="quantity">{followers}</span>
         </li>
-        <li>
+        <li className={css.stats__item}>
           <span className="label">Views</span>
           <span className="quantity">{views}</span>
         </li>
-        <li>
+        <li className={css.stats__item}>
           <span className="label">Likes</span>
           <span className="quantity">{likes}</span>
         </li>
       </ul>
     </div>
   );
+};
+
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  followers: PropTypes.number,
+  views: PropTypes.number,
+  likes: PropTypes.number,
 };
